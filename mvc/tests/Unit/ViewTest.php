842b0917666bcbabd6f04d0fcd857d05c5932b47
Lukasz Urbaniak <urbaniak2016@gmail.com>
2022-06-02 11:05:26 +0200

    Test view

---
+++ mvc/tests/Unit/ViewTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ViewTest extends TestCase
+{
+    public function testViewNotFoundException(): void
+    {
+
+    }
+}
